3d632b767968f9c49122c5982e939ad1312349ba
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-08-22 21:44:19 +0300

    Deck class implementation

---
+++ deck.rb
@@ -0,0 +1,23 @@
+class Deck
+  def initialize(card_class)
+    @card_set = []
+    card_class.each_unique_card { |dignity, suit| @card_set << card_class.new(dignity, suit) }
+    @card_set.freeze
+    @card_buffer = @card_set.shuffle
+  end
+
+  def reset
+    self.card_buffer = card_set.shuffle
+    self
+  end
+
+  def give_cards(number)
+    return unless number <= card_buffer.size
+    card_buffer.pop(number)
+  end
+
+  private
+
+  attr_reader :card_set
+  attr_accessor :card_buffer
+end
