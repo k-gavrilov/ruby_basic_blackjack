cb0218dc10ab6508e142903cfadffb3b56d38a3f
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-08-28 20:36:05 +0300

    Make score message conditional

--- player.rb
+++ player.rb
@@ -13,7 +13,7 @@ class Player
   def make_decision(cards, options_str_arr, parameters)
     message = parameters[:message] || "Choose what to do:"
     score = parameters[:score]
-    score_message = "Your score is #{score}\n"
+    score_message = score ? "Your score is #{score}\n" : ""
     cards_message = "Your cards are: " + cards.map(&:to_s).join(", ") + "\n"
     resulting_message = cards_message + score_message + message
     choose_option(resulting_message, options_str_arr)
