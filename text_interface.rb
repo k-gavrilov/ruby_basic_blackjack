bd4fdb61bc0c57d49e1cee7df2229c6252a5a991
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-09-02 13:34:27 +0300

    Re-write text_interface to class

--- text_interface.rb
+++ text_interface.rb
@@ -1,39 +1,31 @@
-module TextInterface
-  def self.included(base)
-    base.send(:include, InstanceMethods)
+class TextInterface
+  def choose_option(message, string_arr)
+    loop do
+      puts message
+      threshold = string_arr.size - 1
+      puts string_arr
+      choice = gets.to_i
+      if choice > threshold
+        puts "Choose from the listed options!"
+        next
+      end
+      return choice
+    end
   end
 
-  module InstanceMethods
-    protected
-
-    def choose_option(message, string_arr)
-      loop do
-        puts message
-        threshold = string_arr.size - 1
-        puts string_arr
-        choice = gets.to_i
-        if choice > threshold
-          puts "Choose from the listed options!"
-          next
-        end
-        return choice
-      end
-    end
-
-    def enter_value(message, reg_exp)
-      loop do
-        puts message
-        user_input = gets.chomp
-        if user_input !~ reg_exp
-          puts "Enter value in requested format!"
-          next
-        end
-        return user_input
-      end
-    end
-
-    def show_message(message)
+  def enter_value(message, reg_exp)
+    loop do
       puts message
+      user_input = gets.chomp
+      if user_input !~ reg_exp
+        puts "Enter value in requested format!"
+        next
+      end
+      return user_input
     end
+  end
+
+  def show_message(message)
+    puts message
   end
 end
